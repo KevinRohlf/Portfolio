e9cca884a7d60404e838cd04d8adea270d04c4fc
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2023-04-26 20:20:32 +0200

    added header & iam components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IAmComponent } from './i-am/i-am.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IAmComponent
   ],
   imports: [
     BrowserModule,
